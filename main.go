c808f735de329e3c725db3d5a7e09b708083d1e7
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-06-13 00:10:34 -0600

    lectura dinamica de archivos de imagen

--- main.go
+++ main.go
@@ -68,33 +68,4 @@ func main() {
 
 	consola.LimpiarConsola()
 	Menu(lEmp, lImg, lCl, cCl)
-
-	/*
-		l1 := imagenes.ListaImg{}
-		l1.Insertar(&imagenes.Imagen{Nombre: "bmo", Capas: 3})
-		l1.Insertar(&imagenes.Imagen{Nombre: "corrin", Capas: 3})
-		l1.Insertar(&imagenes.Imagen{Nombre: "deadpool", Capas: 4})
-		l1.Insertar(&imagenes.Imagen{Nombre: "hulk", Capas: 4})
-		l1.Insertar(&imagenes.Imagen{Nombre: "mario", Capas: 6})
-		l1.Insertar(&imagenes.Imagen{Nombre: "tux", Capas: 3})
-		l1.Mostrar()
-
-		l2 := empleados.ListaEmp{}
-		l2.Insertar(&empleados.Empleado{Id: "6385", Nombre: "Cristian Suy", Cargo: "Ventas", Contrasena: "6385_Ventas"})
-		l2.Insertar(&empleados.Empleado{Id: "5372", Nombre: "Hector Jimenez", Cargo: "Ventas", Contrasena: "5372_Ventas"})
-		l2.Insertar(&empleados.Empleado{Id: "4399", Nombre: "Pablo Velasquez", Cargo: "Diseño", Contrasena: "4399_Diseño"})
-		l2.Insertar(&empleados.Empleado{Id: "1229", Nombre: "Jaquelin Gomez", Cargo: "Diseño", Contrasena: "1229_Diseño"})
-		l2.Insertar(&empleados.Empleado{Id: "3607", Nombre: "Yadira Ruiz", Cargo: "Diseño", Contrasena: "3607_Diseño"})
-		l2.Insertar(&empleados.Empleado{Id: "3518", Nombre: "Paula Fuentes", Cargo: "Ventas", Contrasena: "3518_Ventas"})
-		l2.Mostrar()
-
-		l3 := clientes.ListaCliente{}
-		l3.Insertar(&clientes.Cliente{Id: "4435", Nombre: "Cristian Suy"})
-		l3.Insertar(&clientes.Cliente{Id: "5268", Nombre: "Hector Jimenez"})
-		l3.Insertar(&clientes.Cliente{Id: "2503", Nombre: "Pablo Velasquez"})
-		l3.Insertar(&clientes.Cliente{Id: "4975", Nombre: "Jaquelin Gomez"})
-		l3.Insertar(&clientes.Cliente{Id: "1280", Nombre: "Yadira Ruiz"})
-		l3.Insertar(&clientes.Cliente{Id: "3101", Nombre: "Paula Fuentes"})
-		l3.Mostrar()
-	*/
 }
--- panelUsuario/Usuario.go
+++ panelUsuario/Usuario.go
@@ -1,9 +1,81 @@
 package panelusuario
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"os"
 	"paquete/consola"
+	imagencapas "paquete/imagenCapas"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/fatih/color"
 )
+
+type ArchivoCapa struct {
+	Capa    int
+	Archivo string
+}
+
+func leerArchivos(nombre string) []ArchivoCapa {
+	ruta := "./csv/" + nombre + "/inicial.csv"
+	file, err := os.Open(ruta)
+	if err != nil {
+		color.Red("\n  Error, no se pudo abrir el archivo")
+		return nil
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	_, err = reader.Read()
+	if err != nil {
+		color.Red("\n  Error, no se pudieron leer los encabezados: ")
+		return nil
+	}
+
+	var archivoCapa []ArchivoCapa
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			color.Red("\n  Error, no se pudo leer el archivo: ")
+			return nil
+		}
+
+		layer, err := strconv.Atoi(row[0])
+		if err != nil {
+			color.Red("\n  Error, no se pudo convertir el número de capa: ")
+			return nil
+		}
+
+		archivoCapa = append(archivoCapa, ArchivoCapa{
+			Capa:    layer,
+			Archivo: row[1],
+		})
+	}
+
+	sort.Slice(archivoCapa, func(i, j int) bool {
+		return archivoCapa[i].Capa < archivoCapa[j].Capa
+	})
+
+	return archivoCapa
+}
+
+func generarImagen(img string) {
+	array := leerArchivos(img)
+	imagen := &imagencapas.ListaCapas{}
+	nombre := ""
+	for _, fl := range array {
+		nombre = strings.TrimSuffix(fl.Archivo, ".csv")
+		if fl.Archivo != "config.csv" {
+			imagen.Insertar(imagencapas.LeerCSV("./csv/"+img+"/"+fl.Archivo, nombre))
+		}
+		imagen.GenerarImg(19, 17, 30, 30, img)
+	}
+}
 
 func MenuUsuario(usuario string) {
 	opcion := 0
@@ -13,7 +85,10 @@ func MenuUsuario(usuario string) {
 
 		switch opcion {
 		case 1:
-			fmt.Print("  Algo")
+			img := ""
+			fmt.Print("  Imagen: ")
+			fmt.Scanln(&img)
+			generarImagen(img)
 		case 2:
 			fmt.Println("  Añgp")
 		case 3:
