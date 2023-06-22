6ef73d3e214004b66cfe053bf766a052c23b7f27
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-06-21 23:50:26 -0600

    corrigiendo envio de datos

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,28 +1,34 @@
+api = 'http://localhost:8080/'
+
 function login() {
-    var usuario = document.getElementById('usuario').value;
-    var contrasena = document.getElementById('contrasena').value;
+    let usuario = document.getElementById('usuario').value;
+    let contrasena = document.getElementById('contrasena').value;
 
-    var loginData = {
-        usuario: usuario,
-        contrasena: contrasena
-    };
-
-    fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(loginData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.message) {
-            alert(data.message);
-        } else if (data.error) {
-            alert(data.error);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    if (usuario.replace(' ','') == '' || contrasena.replace(' ','') == ''){
+        alert('Todos los campos son obligatorios')
+    } else{
+        var loginData = {
+            usuario: usuario,
+            contrasena: contrasena
+        };
+    
+        fetch(api+'login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.message) {
+                alert(data.message);
+            } else if (data.error) {
+                alert(data.error);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 }
